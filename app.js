80a39d525c9e8aa071f73a77395061620c32756b
Areeb-Ali <areebithink@gmail.com>
2024-03-02 15:38:37 +0500

    Making commited

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-const emojis = ["😵", "😵", "😇", "😇", "😊", "😊", "❤️", "😎", "🎯", "😍",]
-    var shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1);
+// Initializing Emojis
 
+// Creates an array named emojis containing ten emoji characters.
+const emojis = ["😵", "😵", "😇", "😇", "😊", "😊", "❤️", "😎", "🎯", "😍",]
+// Shuffles the order of the emojis in the emojis array using a random sort function. 
+var shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1);
+
+// Creating the Game Board 
+
+// Starts a loop that iterates through each emoji in the emojis array.
     for (var i = 0; i < emojis.length; i++) {
+        // Creates a new div element representing a box on the game board.
         let box = document.createElement('div')
+        // Assigns a class name of item to the box for styling purposes.
         box.className = 'item';
+        // Sets the inner HTML of the box to the corresponding emoji from the shuffled shuf_emojis array.
         box.innerHTML = shuf_emojis[i]
+        // Attaches a click event listener to the box, triggering a function when it's clicked.
         box.onclick = function () {
+
+            // Handling Click Events
+
+            // Adds the class boxOpen to the clicked box, visually indicating it's open.
             this.classList.add('boxOpen')
+            // Delays the execution of the next function by 500 milliseconds (half a second).
             setTimeout(function () {
+
+                // Checking for Matches (delayed function) 
+
+                // Checks if more than one box is currently open.
                 if (document.querySelectorAll('.boxOpen').length > 1) {
+                    // If two boxes are open:
                     if (document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML) {
                         document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch')
                         document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch')
